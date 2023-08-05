0bfd45cefd712bdbb330401b59984023f1ff2d48
Yudhajit Chowdhury <110385709+berryBatKnave@users.noreply.github.com>
2023-08-05 15:42:43 +0530

    ToDo List Application v1.0
    Created a website which keeps track of the to-list items that are created by the user.
    
    The basic functionality of the website has been made so releasing the website with the version of 1.0.
    
    There is a minor bug in the website that allows user to refresh the /submit page and the array which is storing the elements which are added gets pushed again with the same value as the last one. So in short a duplicate entry gets created.
    
    Will be tryin to fix the bug in the later releases.Currently I am not sure how to restric the user to refresh the /submit page.
    
    Adding Functionalities : Will be adding the functionality of a delete button when I will fix the bug for stopping refresh.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+var arr = [];
 
 const app = express();
 const port = 3000;
@@ -8,11 +9,19 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  arr = [];
   res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
-  
+  var v = req.body["list-value"];
+  if(v===""){
+    console.log("Nothing To Add!");
+  }
+  else{
+    arr.push(v);
+    res.render("index.ejs", { listArray: arr });
+  }
 });
 
 app.listen(port, () => {
